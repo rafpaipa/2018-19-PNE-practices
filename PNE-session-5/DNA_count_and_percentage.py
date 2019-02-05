5c4f3fdbcb96ae13627933efe9e84ea8b8f66b81
rafpaipa <rafpaipa@hotmail.com>
2019-02-05 16:07:14 +0100

    session 1

---
+++ PNE-session-5/DNA_count_and_percentage.py
@@ -0,0 +1,25 @@
+seq = input( "Please enter a DNA sequence: " )
+
+number_A = 0
+number_T = 0
+number_G = 0
+number_C = 0
+
+for i in range(len(seq)):
+   if seq[i] == "A":
+       number_A = number_A + 1
+   if seq[i] == "C":
+       number_C = number_C + 1
+   if seq[i] == "T":
+       number_T = number_T + 1
+   if seq[i] == "G":
+       number_G = number_G + 1
+print("The number of 'A' bases is:", number_A)
+print("The number of 'T' bases is:", number_T)
+print("The number of 'C' bases is:", number_C)
+print("The number of 'G' bases is:", number_G)
+
+print("The percentage of 'A' basis is:", number_A/len(seq)*100, "%")
+print("The percentage of 'T' basis is:", number_T/len(seq)*100, "%")
+print("The percentage of 'C' basis is:", number_C/len(seq)*100, "%")
+print("The percentage of 'G' basis is:", number_G/len(seq)*100, "%")
